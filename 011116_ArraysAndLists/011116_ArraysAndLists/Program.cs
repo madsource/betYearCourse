9a85f5d930c83652ecae114fe44ece6ee78a1109
madsource <chocho1006@abv.bg>
2016-11-01 15:01:34 +0200

    First task for lists.

---
+++ 011116_ArraysAndLists/011116_ArraysAndLists/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _011116_ArraysAndLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var madList = new List<String>();
+
+            madList.Add("1Baba");
+            madList.Add("xBaba2");
+            madList.Add("Baba3");
+
+            Visualize(madList);
+
+            madList.Sort();
+
+            Visualize(madList);
+        }
+
+        static void Visualize(List<string> list)
+        {
+            System.Console.WriteLine("Start =========\n");
+
+            foreach (var l in list)
+            {
+                System.Console.WriteLine(l);
+            }
+
+            System.Console.WriteLine("\nEnd   =========\n");
+        }
+    }
+}
