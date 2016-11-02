6a3cfd8ead0fac93ee1b747c348dcf27ea1c231f
madsource <chocho1006@abv.bg>
2016-11-02 15:58:09 +0200

    Task Persons - improvements

--- 021116/PersonTask/Program.cs
+++ 021116/PersonTask/Program.cs
@@ -11,28 +11,29 @@ namespace PersonTask
         static void Main(string[] args)
         {
             // Constructors testing
-            Console.Write("Give me name: ");
-            string personName = Console.ReadLine();
-            Console.Write("Give me age: ");
-            int personAge;
-            int.TryParse(Console.ReadLine(), out personAge);
+            //Console.Write("Give me name: ");
+            //string personName = Console.ReadLine();
+            //Console.Write("Give me age: ");
+            //int personAge;
+            //int.TryParse(Console.ReadLine(), out personAge);
 
-            Person p1 = new Person();
-            Person p2 = new Person(personName);
-            Person p3 = new Person(personName, personAge);
+            //Person p1 = new Person();
+            //Person p2 = new Person(personName);
+            //Person p3 = new Person(personName, personAge);
 
-            Console.Write(p1.Name + " ");
-            Console.Write(p1.Age + "\n");
+            //Console.Write(p1.Name + " ");
+            //Console.Write(p1.Age + "\n");
 
-            Console.Write(p2.Name + " ");
-            Console.Write(p2.Age + "\n");
+            //Console.Write(p2.Name + " ");
+            //Console.Write(p2.Age + "\n");
 
-            Console.Write(p3.Name + " ");
-            Console.Write(p3.Age + "\n\n");
+            //Console.Write(p3.Name + " ");
+            //Console.Write(p3.Age + "\n\n");
 
             // Object sorting by length of name, excluding age under 18
             List<Person> personsList = new List<Person>();
 
+            Console.WriteLine("Please, enter people by name//age and \"quit\" to quit.");
             while(true)
             {
                 string input = Console.ReadLine();
@@ -51,14 +52,14 @@ namespace PersonTask
 
             if(personsList.Count > 0)
             {
-                personsList.OrderBy(p => p.Name.Length);
-                foreach (var p in personsList)
+                List<Person> orderedList = personsList.OrderBy(p => p.Name.Length).ToList();
+                foreach (var p in orderedList)
                 {
                     Console.WriteLine($"{p.Name} - {p.Age}");
                 }
             } else
             {
-                Console.WriteLine("No persons ion your list!");
+                Console.WriteLine("No persons in your list!");
             }
         }
     }
