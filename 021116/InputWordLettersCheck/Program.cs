07f72c61c3bb42e0a555bfab3079dbe87f6da142
madsource <chocho1006@abv.bg>
2016-11-02 12:25:56 +0200

    Word with letters or underscores only

---
+++ 021116/InputWordLettersCheck/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InputWordLettersCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a word <only letters> : ");
+            string word = Console.ReadLine();
+
+            if (!word.Trim().All(c => Char.IsLetter(c) || c == '_'))
+            {
+                Console.WriteLine("This word CONTAINS not allowed symbols!\nPlease enter only letters.");
+            }
+            else
+            {
+                Console.WriteLine("Yeah, good word!");
+            }            
+        }
+    }
+}
