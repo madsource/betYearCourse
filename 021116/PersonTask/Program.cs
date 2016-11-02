faa604f93ad640b8f8b3e9bbfcd048297f5c7bb9
madsource <chocho1006@abv.bg>
2016-11-02 15:43:03 +0200

    Person class tasks

---
+++ 021116/PersonTask/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Constructors testing
+            Console.Write("Give me name: ");
+            string personName = Console.ReadLine();
+            Console.Write("Give me age: ");
+            int personAge;
+            int.TryParse(Console.ReadLine(), out personAge);
+
+            Person p1 = new Person();
+            Person p2 = new Person(personName);
+            Person p3 = new Person(personName, personAge);
+
+            Console.Write(p1.Name + " ");
+            Console.Write(p1.Age + "\n");
+
+            Console.Write(p2.Name + " ");
+            Console.Write(p2.Age + "\n");
+
+            Console.Write(p3.Name + " ");
+            Console.Write(p3.Age + "\n\n");
+
+            // Object sorting by length of name, excluding age under 18
+            List<Person> personsList = new List<Person>();
+
+            while(true)
+            {
+                string input = Console.ReadLine();
+                if (input == "quit") break;
+
+                string[] personsDetails = input.Split(new string[] { "//" }, StringSplitOptions.RemoveEmptyEntries);
+                string personsName = personsDetails[0];
+                int personsAge = int.Parse(personsDetails[1]);
+
+                if (personsAge >= 18 )
+                {
+                    Person person = new Person(personsName, personsAge);
+                    personsList.Add(person);
+                }
+            }
+
+            if(personsList.Count > 0)
+            {
+                personsList.OrderBy(p => p.Name.Length);
+                foreach (var p in personsList)
+                {
+                    Console.WriteLine($"{p.Name} - {p.Age}");
+                }
+            } else
+            {
+                Console.WriteLine("No persons ion your list!");
+            }
+        }
+    }
+}
