491b65991c65789d78d9e6a9df0b0a15e0edd380
madsource <chocho1006@abv.bg>
2016-11-09 16:11:42 +0200

    Files IO

---
+++ 021116/Files/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter writer = new StreamWriter("../../text.txt", true, Encoding.UTF8);
+
+            //writer.Close(); -- using instead! Self closing.
+
+            using (writer)
+            {
+                writer.WriteLine("Лол!");
+                writer.WriteLine("Hey! Yey!");
+            }
+
+            StreamReader reader = new StreamReader("../../text.txt", Encoding.UTF8);
+
+            using (reader)
+            {
+                //string text = reader.ReadToEnd();
+                //Console.WriteLine(text);
+
+                //or 
+
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+
+
+            using (BinaryWriter writerBin = new BinaryWriter(File.Open("../../binary.bin", FileMode.Create)))
+            {
+                writerBin.Write(28);
+            }
+
+            using (BinaryReader readerBin = new BinaryReader(File.Open("../../binary.bin", FileMode.Open)))
+            {
+                int integerValue = readerBin.ReadInt32();
+                Console.WriteLine(integerValue);
+            }
+        }
+    }
+}
