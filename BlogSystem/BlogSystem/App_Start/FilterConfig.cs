c9c0304e80ea3b9ef3d208897be42028553a5a61
madsource <chocho1006@abv.bg>
2016-11-23 16:04:45 +0200

    Delete of post done.

--- BlogSystem/BlogSystem/App_Start/FilterConfig.cs
+++ BlogSystem/BlogSystem/App_Start/FilterConfig.cs
@@ -9,7 +9,7 @@ namespace BlogSystem
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
-            filters.Add(new NotFoundRedirect());
+            //filters.Add(new NotFoundRedirect());
         }
     }
 }
--- BlogSystem/BlogSystem/Controllers/AddBlogPostController.cs
+++ BlogSystem/BlogSystem/Controllers/AddBlogPostController.cs
@@ -6,18 +6,12 @@ using BlogSystem.Models;
 using BlogSystem.ViewModels;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System.Net;
 
 namespace BlogSystem.Controllers
 {
-    public class AddBlogPostController : Controller
+    public class AddBlogPostController : RootController
     {
-        private BlogSystemDbContext dbContext;
-        private UserManager<ApplicationUser> UserManager;
-        public AddBlogPostController()
-        {
-            this.dbContext = new BlogSystemDbContext();
-            this.UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(this.dbContext));
-        }
 
         [HttpGet]
         [Authorize]
@@ -26,14 +20,14 @@ namespace BlogSystem.Controllers
             PostViewModel postViewModel = new PostViewModel();
             if (id != null)
             {
-                var postModel = dbContext.Posts.FirstOrDefault(p => p.Id == id);
+                var postModel = BlogSystemDbContext.Posts.FirstOrDefault(p => p.Id == id);
                 postViewModel.Name = postModel.Name;
                 postViewModel.Content = postModel.Content;
                 postViewModel.DateCreated = postModel.DateCreated;
                 postViewModel.Id = postModel.Id;
                 postViewModel.Username = postModel.User.UserName;
             }
-  
+
             return View(postViewModel);
         }
 
@@ -41,15 +35,16 @@ namespace BlogSystem.Controllers
         [Authorize]
         public ActionResult UpdateBlogPost(PostViewModel postViewModel)
         {
-            if(postViewModel.Id != 0)
+            if (postViewModel.Id != 0)
             {
-                var existingPost = this.dbContext.Posts.FirstOrDefault(p => p.Id == postViewModel.Id);
+                var existingPost = this.BlogSystemDbContext.Posts.FirstOrDefault(p => p.Id == postViewModel.Id);
                 existingPost.Name = postViewModel.Name;
                 existingPost.Content = postViewModel.Content;
                 existingPost.DateCreated = DateTime.Now;
                 existingPost.User = UserManager.FindById(User.Identity.GetUserId());
 
-            } else
+            }
+            else
             {
                 var newPost = new Models.Post()
                 {
@@ -58,12 +53,74 @@ namespace BlogSystem.Controllers
                     DateCreated = DateTime.Now,
                     User = UserManager.FindById(User.Identity.GetUserId())
                 };
-                this.dbContext.Posts.Add(newPost);
+                this.BlogSystemDbContext.Posts.Add(newPost);
             }
-            
-            dbContext.SaveChanges();
+
+            BlogSystemDbContext.SaveChanges();
 
             return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public ActionResult PreviewBlogPost(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            else
+            {
+                var post = this.GetPost(id);
+                return View(post);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult DeleteBlogPost(int? id)
+        {
+
+            return RedirectToAction("PreviewBlogPost");
+
+        }
+
+        [HttpPost, ActionName("DeleteBlogPost")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var existingPost = this.BlogSystemDbContext.Posts.FirstOrDefault(p => p.Id == id);
+
+            var postName = existingPost.Name;
+            var postAuthor = existingPost.User.UserName;
+
+            if (existingPost == null)
+            {
+                return HttpNotFound();
+            }
+
+            this.BlogSystemDbContext.Posts.Remove(existingPost);
+            this.BlogSystemDbContext.SaveChanges();
+
+            return RedirectToAction("Index", new { deletedName = postName, deletedAuthor = postAuthor });
+        }
+
+        private PostViewModel GetPost(int? id)
+        {
+            var existingPost = this.BlogSystemDbContext.Posts.FirstOrDefault(p => p.Id == id);
+
+            if (existingPost == null)
+            {
+                return null;
+            }
+
+            PostViewModel postViewModel = new PostViewModel()
+            {
+                Name = existingPost.Name,
+                Content = existingPost.Content,
+                DateCreated = existingPost.DateCreated,
+                Username = existingPost.User.UserName,
+                Id = existingPost.Id
+            };
+
+            return postViewModel;
         }
     }
 }
