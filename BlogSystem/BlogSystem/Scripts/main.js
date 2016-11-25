269a02c8d274c6603bc8eefce53af3b5896fbe6c
madsource <chocho1006@abv.bg>
2016-11-25 11:21:23 +0200

    edit comments ajax form - js refactor to bind the click event to appended new comments.

--- BlogSystem/BlogSystem/Scripts/main.js
+++ BlogSystem/BlogSystem/Scripts/main.js
@@ -1,12 +1,12 @@
 ﻿$(document).ready(function () {
     
-    $(".editComment").click(function (e) {
+    $("#comments").on("click",".editComment",function (e) {
         e.preventDefault();
         $(this).siblings(".editForm").show();
         console.log($(this).find(".editForm"));
     });
 
-    $(".editForm > .close").click(function (e) {
+    $("#comments").on("click", ".editForm > .close", function (e) {
         e.preventDefault();
         $(this).parent().hide();
     });
